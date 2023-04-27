a0a9ed51f6cd5067520710e3f383f49a4d51f7a7
augustus-0 <113288678+augustus-0@users.noreply.github.com>
2023-04-27 16:55:46 +0800

    Update streamlit_app.py
    Added missing 'except'

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,8 @@ try:
     streamlit.dataframe(fruityvice_normalized)
     # output it as a table
     streamlit.dataframe(fruityvice_normalized)
-
+except URLError as e:
+  streamlit.error()
 
 
 #Troubleshooting fruityvice section
